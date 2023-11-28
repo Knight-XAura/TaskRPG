8abedf78ba348c4a8316b45c2b169c49e2a9b052
Knight-XAura <shawn.a.harriman@outlook.com>
2023-11-28 18:42:48 -0500

    Removed groups and worked on fixing HUD UI

--- Main.gd
+++ Main.gd
@@ -4,11 +4,21 @@ extends VBoxContainer
 
 var tasks_scene: PackedScene = preload("res://menu/tasks/tasks.tscn")
 
-@onready var current_tab: Control = svp_container
+enum MENU_TABS {
+	SVPCONTAINER = 0,
+	TASKS = 1
+}
+
+var current_tab: int = MENU_TABS.SVPCONTAINER
+
+@onready var cached_tab: Control = svp_container
 
 func _on_tasks_pressed() -> void:
-	var tasks: VBoxContainer = tasks_scene.instantiate()
-	add_child(tasks, true) #Need to make this move or be added to the top above or below SVPContainer
-	current_tab.hide()
-	current_tab = tasks
-	
+	if current_tab != MENU_TABS.TASKS:
+		var tasks: VBoxContainer = tasks_scene.instantiate()
+		add_child(tasks, true)
+		move_child(tasks, 1)
+		cached_tab.hide()
+		cached_tab = tasks
+		current_tab = MENU_TABS.TASKS
+# This needs more work with the variables and see if we can remove move_child
