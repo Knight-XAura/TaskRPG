f7e5910be996b5a33bb34269d33e2ec2d0027f1f
Knight-XAura <shawn.a.harriman@outlook.com>
2023-11-28 22:21:42 -0500

    Refactoring Main

--- Main.gd
+++ Main.gd
@@ -1,24 +1,31 @@
 extends VBoxContainer
 
-@onready var svp_container: SubViewportContainer = $SVPContainer
 
-var tasks_scene: PackedScene = preload("res://menu/tasks/tasks.tscn")
+@onready var view: Control = $View
+@onready var world: SubViewportContainer = $View/SVPContainer
+
+var tab_scenes: Array[PackedScene] = [null, preload("res://menu/tasks/tasks.tscn"), null]
 
 enum MENU_TABS {
-	SVPCONTAINER = 0,
+	WORLD = 0,
 	TASKS = 1
 }
 
-var current_tab: int = MENU_TABS.SVPCONTAINER
+var current_tab_eid: MENU_TABS = MENU_TABS.WORLD
 
-@onready var cached_tab: Control = svp_container
+@onready var current_tab: Control = world
 
-func _on_tasks_pressed() -> void:
-	if current_tab != MENU_TABS.TASKS:
-		var tasks: VBoxContainer = tasks_scene.instantiate()
-		add_child(tasks, true)
-		move_child(tasks, 1)
-		cached_tab.hide()
-		cached_tab = tasks
-		current_tab = MENU_TABS.TASKS
-# This needs more work with the variables and see if we can remove move_child
+
+func _on_menu_pressed(new_tab_eid: int) -> void:
+	if current_tab_eid != new_tab_eid:
+		var tab_to_hide: Control = current_tab
+		var tab_to_show = tab_scenes[new_tab_eid].instantiate() as Control if tab_scenes[new_tab_eid] else world
+		if tab_to_show != world:
+			view.add_child(tab_to_show, true)
+		current_tab = tab_to_show
+		current_tab_eid = new_tab_eid
+		if current_tab_eid == MENU_TABS.WORLD:
+			current_tab.show()
+		tab_to_hide.hide()
+		if tab_to_hide != world:
+			tab_to_hide.queue_free()
