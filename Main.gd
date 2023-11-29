0028d7c86f31b59d6208d478e967835a58e16d6f
Knight-XAura <shawn.a.harriman@outlook.com>
2023-11-29 18:42:13 -0500

    More refactorng and some bug fixes

--- Main.gd
+++ Main.gd
@@ -1,10 +1,13 @@
 extends VBoxContainer
 
 
-@onready var view: Control = $View
-@onready var world: SubViewportContainer = $View/SVPContainer
+@onready var view: Control = $View as Control
+@onready var world: SubViewportContainer = $View/SVPContainer as SubViewportContainer
+@onready var current_tab: Control = world
 
-const TAB_SCENES: Array[PackedScene] = [
+var current_tab_eid: MAIN_NAVIGATION_TABS = MAIN_NAVIGATION_TABS.WORLD
+
+const MAIN_NAVIGATION_SCENES: Array[PackedScene] = [
 	null,
 	preload("res://menu/tasks/tasks.tscn"),
 	preload("res://menu/stats/stats.tscn"),
@@ -12,7 +15,7 @@ const TAB_SCENES: Array[PackedScene] = [
 	preload("res://menu/settings/settings.tscn")
 ]
 
-enum MENU_TABS {
+enum MAIN_NAVIGATION_TABS {
 	WORLD = 0,
 	TASKS = 1,
 	STATS = 2,
@@ -20,21 +23,27 @@ enum MENU_TABS {
 	SETTINGS = 4
 }
 
-var current_tab_eid: MENU_TABS = MENU_TABS.WORLD
 
-@onready var current_tab: Control = world
+func _init() -> void:
+	pass # Need code to pull last saved data
 
 
-func _on_navigation_bar_tab_pressed(new_tab_eid: int) -> void:
+func _on_main_navigation_bar_pressed(new_tab_eid: int) -> void:
 	if current_tab_eid != new_tab_eid:
 		var tab_to_hide: Control = current_tab
-		var tab_to_show = TAB_SCENES[new_tab_eid].instantiate() as Control if TAB_SCENES[new_tab_eid] else world
+		var tab_to_show = MAIN_NAVIGATION_SCENES[new_tab_eid].instantiate() as Control \
+				if MAIN_NAVIGATION_SCENES[new_tab_eid] != null \
+				else world
 		if tab_to_show != world:
 			view.add_child(tab_to_show, true)
 		current_tab = tab_to_show
-		current_tab_eid = new_tab_eid
-		if current_tab_eid == MENU_TABS.WORLD:
+		current_tab_eid = new_tab_eid as MAIN_NAVIGATION_TABS
+		if current_tab_eid == MAIN_NAVIGATION_TABS.WORLD:
 			current_tab.show()
 		tab_to_hide.hide()
 		if tab_to_hide != world:
 			tab_to_hide.queue_free()
+
+
+func _exit_tree() -> void:
+	pass # Save data to file
--- menu/tasks/tasks.gd
+++ menu/tasks/tasks.gd
@@ -1,23 +1,49 @@
 extends VBoxContainer
 
 
-@onready var view_scroll_container: ScrollContainer = $ViewScrollContainer
+@onready var task_view: ScrollContainer = $View as ScrollContainer
 
-const TASK_SCENES: Array[PackedScene] = [
+var current_tab_eid: TASK_NAVIGATION_TABS
+var current_tab: Control
+
+# Maybe an array or even a dictionary here?
+var previously_open_tab_scene: PackedScene
+var previously_open_tab: Control
+
+const TASK_NAVIGATION_SCENES: Array[PackedScene] = [
 	preload("res://menu/tasks/habit/habit.tscn"),
 	preload("res://menu/tasks/daily/daily.tscn"),
 	preload("res://menu/tasks/todo/todo.tscn")
 ]
 
-enum TASK_TABS {
+enum TASK_NAVIGATION_TABS {
 	HABITS = 0,
 	DAILIES = 1,
 	TODOS = 2
 }
 
-var current_tab_eid: TASK_TABS
-var current_tab: Control
+func _init() -> void:
+	pass # Need code to pull last saved tab
 
 
-func _on_task_bar_tab_pressed(new_tab_eid: int) -> void:
-	pass
+func _ready() -> void:
+	if previously_open_tab != null:
+		previously_open_tab = previously_open_tab_scene.instantiate()
+		task_view.add_child(previously_open_tab, true) # Need to assign tabs eid to current_tab_eid
+		current_tab = previously_open_tab
+
+# Might be able to emit signals to improve this and the _ready function
+func _on_task_navigation_bar_pressed(new_tab_eid: int) -> void:
+	if current_tab_eid != new_tab_eid:
+		var tab_to_hide: Control = current_tab
+		var tab_to_show = TASK_NAVIGATION_SCENES[new_tab_eid].instantiate() as Control
+		task_view.add_child(tab_to_show, true)
+		current_tab = tab_to_show
+		current_tab_eid = new_tab_eid as TASK_NAVIGATION_TABS
+		if previously_open_tab != null:
+			tab_to_hide.hide()
+			tab_to_hide.queue_free()
+
+
+func _exit_tree() -> void:
+	pass # Save previously open tab to file or even keep in ram somewhere else and save later
