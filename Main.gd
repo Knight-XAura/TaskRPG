5f0c671f61a7135330d2a2bdeebf8df13bd44fe2
Knight-XAura <shawn.a.harriman@outlook.com>
2023-11-30 00:13:15 -0500

    More Refactoring and broke some stuff, working on fixing

--- Main.gd
+++ Main.gd
@@ -6,14 +6,21 @@ extends VBoxContainer
 @onready var current_tab: Control = world
 
 var current_tab_eid: MAIN_NAVIGATION_TABS = MAIN_NAVIGATION_TABS.WORLD
-
-const MAIN_NAVIGATION_SCENES: Array[PackedScene] = [
-	null,
-	preload("res://menu/tasks/tasks.tscn"),
-	preload("res://menu/stats/stats.tscn"),
-	preload("res://menu/inventory/inventory.tscn"),
-	preload("res://menu/settings/settings.tscn")
-]
+# Need to fix this not able to use if statement to pull what set of data I want (making type Array fixes this, but isn't as strongly statically typed
+var main_navigation_scenes: Array[PackedScene] = [
+		null,
+		load("user://user_tasks.scn"),
+		preload("res://menu/stats/stats.tscn"),
+		preload("res://menu/inventory/inventory.tscn"),
+		preload("res://menu/settings/settings.tscn")
+]	if FileAccess.open("user://user_tasks.scn", FileAccess.READ) != null \
+	else [
+		null,
+		preload("res://menu/tasks/tasks.tscn"),
+		preload("res://menu/stats/stats.tscn"),
+		preload("res://menu/inventory/inventory.tscn"),
+		preload("res://menu/settings/settings.tscn")
+	]
 
 enum MAIN_NAVIGATION_TABS {
 	WORLD = 0,
@@ -27,17 +34,19 @@ enum MAIN_NAVIGATION_TABS {
 func _init() -> void:
 	pass # Need code to pull last saved data
 
-
-func _on_main_navigation_bar_pressed(new_tab_eid: int) -> void:
-	if current_tab_eid != new_tab_eid:
+# Signals disappeared on me again
+func _on_main_navigation_bar_pressed(tab_to_show_eid: int) -> void:
+	if current_tab_eid != tab_to_show_eid:
 		var tab_to_hide: Control = current_tab
-		var tab_to_show = MAIN_NAVIGATION_SCENES[new_tab_eid].instantiate() as Control \
-				if MAIN_NAVIGATION_SCENES[new_tab_eid] != null \
-				else world
+		var tab_to_show = main_navigation_scenes[tab_to_show_eid].instantiate() as Control \
+			if main_navigation_scenes[tab_to_show_eid] != null \
+			else world
 		if tab_to_show != world:
 			view.add_child(tab_to_show, true)
 		current_tab = tab_to_show
-		current_tab_eid = new_tab_eid as MAIN_NAVIGATION_TABS
+		current_tab_eid = tab_to_show_eid as MAIN_NAVIGATION_TABS
+		if current_tab_eid == MAIN_NAVIGATION_TABS.TASKS:
+			current_tab.user_tasks_updated.connect("_on_user_tasks_updated")
 		if current_tab_eid == MAIN_NAVIGATION_TABS.WORLD:
 			current_tab.show()
 		tab_to_hide.hide()
@@ -47,3 +56,8 @@ func _on_main_navigation_bar_pressed(new_tab_eid: int) -> void:
 
 func _exit_tree() -> void:
 	pass # Save data to file
+
+
+func _on_user_tasks_updated() -> void:
+	main_navigation_scenes[MAIN_NAVIGATION_TABS.TASKS] = load("user://user_tasks.scn")
+	print("UPDATED")
--- menu/tasks/tasks.gd
+++ menu/tasks/tasks.gd
@@ -1,5 +1,6 @@
 extends VBoxContainer
 
+signal user_tasks_updated
 
 @onready var task_view: ScrollContainer = $View as ScrollContainer
 
@@ -10,7 +11,7 @@ var current_tab: Control
 var previously_open_tab_scene: PackedScene
 var previously_open_tab: Control
 
-const TASK_NAVIGATION_SCENES: Array[PackedScene] = [
+const TASK_SCENES: Array[PackedScene] = [
 	preload("res://menu/tasks/habit/habit.tscn"),
 	preload("res://menu/tasks/daily/daily.tscn"),
 	preload("res://menu/tasks/todo/todo.tscn")
@@ -25,25 +26,31 @@ enum TASK_NAVIGATION_TABS {
 func _init() -> void:
 	pass # Need code to pull last saved tab
 
-
+# Below this point needs reconstructing
+# Also need to figure out tasks for inheritance scenes vs scene containing users tasks
 func _ready() -> void:
 	if previously_open_tab != null:
-		previously_open_tab = previously_open_tab_scene.instantiate()
-		task_view.add_child(previously_open_tab, true) # Need to assign tabs eid to current_tab_eid
-		current_tab = previously_open_tab
+		current_tab = previously_open_tab # Need to finish completing this
 
 # Might be able to emit signals to improve this and the _ready function
 func _on_task_navigation_bar_pressed(new_tab_eid: int) -> void:
 	if current_tab_eid != new_tab_eid:
 		var tab_to_hide: Control = current_tab
-		var tab_to_show = TASK_NAVIGATION_SCENES[new_tab_eid].instantiate() as Control
-		task_view.add_child(tab_to_show, true)
-		current_tab = tab_to_show
+		#current_tab = tab_to_show
 		current_tab_eid = new_tab_eid as TASK_NAVIGATION_TABS
-		if previously_open_tab != null:
+		if previously_open_tab != null: # Same as tab_to_hide node check
 			tab_to_hide.hide()
-			tab_to_hide.queue_free()
 
 
 func _exit_tree() -> void:
-	pass # Save previously open tab to file or even keep in ram somewhere else and save later
+	get_children_recursively(self)
+	var user_tasks_scene: PackedScene = PackedScene.new()
+	user_tasks_scene.pack(self)
+	ResourceSaver.save(user_tasks_scene, "user://user_tasks.scn")
+	user_tasks_updated.emit()
+
+
+func get_children_recursively(parent: Node) -> void:
+	for node in parent.get_children():
+		get_children_recursively(node)
+		node.owner = self
