f9e4ca009f19b87e706fd5cf30f32640b6eb766d
Knight-XAura <shawn.a.harriman@outlook.com>
2023-11-30 20:56:36 -0500

    Further adjustments to straighten out Tasks and Main Scenes

--- Main.gd
+++ Main.gd
@@ -8,19 +8,12 @@ extends VBoxContainer
 var current_tab_eid: MAIN_NAVIGATION_TABS = MAIN_NAVIGATION_TABS.WORLD
 # Need to fix this not able to use if statement to pull what set of data I want (making type Array fixes this, but isn't as strongly statically typed
 var main_navigation_scenes: Array[PackedScene] = [
-		null,
-		load("user://user_tasks.scn"),
-		preload("res://menu/stats/stats.tscn"),
-		preload("res://menu/inventory/inventory.tscn"),
-		preload("res://menu/settings/settings.tscn")
-]	if FileAccess.open("user://user_tasks.scn", FileAccess.READ) != null \
-	else [
-		null,
-		preload("res://menu/tasks/tasks.tscn"),
-		preload("res://menu/stats/stats.tscn"),
-		preload("res://menu/inventory/inventory.tscn"),
-		preload("res://menu/settings/settings.tscn")
-	]
+	null,
+	load("res://menu/tasks/tasks.tscn"),
+	load("res://menu/stats/stats.tscn"),
+	load("res://menu/inventory/inventory.tscn"),
+	load("res://menu/settings/settings.tscn")
+]
 
 enum MAIN_NAVIGATION_TABS {
 	WORLD = 0,
@@ -32,7 +25,8 @@ enum MAIN_NAVIGATION_TABS {
 
 
 func _init() -> void:
-	pass # Need code to pull last saved data
+	if FileAccess.open("user://user_tasks.scn", FileAccess.READ) != null:
+		main_navigation_scenes[MAIN_NAVIGATION_TABS.TASKS] = load("res://menu/tasks/tasks.tscn")
 
 # Signals disappeared on me again
 func _on_main_navigation_bar_pressed(tab_to_show_eid: int) -> void:
@@ -46,6 +40,7 @@ func _on_main_navigation_bar_pressed(tab_to_show_eid: int) -> void:
 		current_tab = tab_to_show
 		current_tab_eid = tab_to_show_eid as MAIN_NAVIGATION_TABS
 		if current_tab_eid == MAIN_NAVIGATION_TABS.TASKS:
+			print(current_tab)
 			current_tab.user_tasks_updated.connect("_on_user_tasks_updated")
 		if current_tab_eid == MAIN_NAVIGATION_TABS.WORLD:
 			current_tab.show()
